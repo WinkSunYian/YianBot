0630d82949393386132355340ff91a95cf16d10b
SunYian <winksunyian@gmail.com>
2024-11-20 16:05:16 +0800

    modified:   YianBot/plugins/Preprocessor/__init__.py

--- YianBot/plugins/Preprocessor/__init__.py
+++ YianBot/plugins/Preprocessor/__init__.py
@@ -7,9 +7,22 @@ from nonebot.adapters.onebot.v11 import (
     Bot,
     MessageEvent,
     MessageSegment,
+    PrivateMessageEvent,
 )
-from utils.utils import ConfigReader
+from utils.utils import ConfigReader, DailyCountLimiter
 from .data_source import get_user_tags, is_tag_present, get_user_tags_or_create
+
+count = DailyCountLimiter(1)
+
+
+@event_preprocessor
+async def _(bot: Bot, event: PrivateMessageEvent):
+    if count.check(event.user_id):
+        count.increase(event.user_id)
+        await bot.send(
+            event=event,
+            message="广告内容",
+        )
 
 
 @event_preprocessor
@@ -17,7 +30,7 @@ async def _(event: MessageEvent):
     if event.is_tome():
         tag_list = await get_user_tags_or_create(event.user_id)
         # if is_tag_present(tag_list, "banned"):
-            # raise IgnoredException("黑名单用户")
+        # raise IgnoredException("黑名单用户")
 
 
 @run_preprocessor
