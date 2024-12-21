ad3dd138070fc9304e160d0f066a433528c9a9e6
SunYian <winksunyian@gmail.com>
2024-12-21 23:43:25 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   plugins/Robbery/__init__.py

--- plugins/Robbery/__init__.py
+++ plugins/Robbery/__init__.py
@@ -20,7 +20,7 @@ robbery = on_command("打劫", aliases={"抢劫"}, priority=6, block=True)
 
 @robbery.handle()
 async def _(event: MessageEvent, args: Message = CommandArg()):
-    args_list = args_split(args, 1)
+    args_list = args_split(args)
     """
     args_list[0] : QQ
     """
