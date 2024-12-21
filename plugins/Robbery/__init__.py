369884077ee0f1874198bd1cd8ead5dbd3fff12d
SunYian <winksunyian@gmail.com>
2024-12-21 22:31:47 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   plugins/Robbery/__init__.py

--- plugins/Robbery/__init__.py
+++ plugins/Robbery/__init__.py
@@ -7,16 +7,11 @@ from nonebot.adapters.onebot.v11 import (
     MessageEvent,
     PrivateMessageEvent,
 )
-from nonebot.params import (
-    CommandArg
-)
-from utils.utils import (
-    DailyCountLimiter,
-    BackpackControl
-)
-from utils.utils import FreqLimiter, args_split
+from nonebot.params import CommandArg
 from random import choice
-from .data_source import play
+from utils.utils import DailyCountLimiter, BackpackControl
+from utils.utils import FreqLimiter, args_split
+from utils.http_utils import http_client
 
 cd = FreqLimiter(60)
 
@@ -32,29 +27,26 @@ async def _(event: MessageEvent, args: Message = CommandArg()):
     if len(args_list) != 1:
         await robbery.finish(MessageSegment.reply(event.message_id) + "缺少参数[QQ]")
     elif not isinstance(args_list[0], int):
-        await robbery.finish(MessageSegment.reply(event.message_id) + "[QQ]应该是一个At对象,或整数")
+        await robbery.finish(
+            MessageSegment.reply(event.message_id) + "[QQ]应该是一个At对象,或整数"
+        )
     elif not (5 <= len(str(args_list[0])) <= 10):
-        await robbery.finish(MessageSegment.reply(event.message_id) + "[QQ]的长度为5-10")
+        await robbery.finish(
+            MessageSegment.reply(event.message_id) + "[QQ]的长度为5-10"
+        )
     elif args_list[0] == event.user_id:
         await robbery.finish(MessageSegment.reply(event.message_id) + "打劫自己是吧")
     else:
         if cd.check(event.user_id):
             cd.start_cd(event.user_id)
-            my_backpack = UserBackpackManager(event.user_id)
-            target_backpack = UserBackpackManager(args_list[0])
-            money = play(my_backpack["软妹币"], target_backpack["软妹币"])
-            if money == 0:
-                msg = "打劫路上被叔叔抓走了"
-            elif money > 0:
-                msg = f"打劫成功,夺取了对方软妹币 * {money}"
-                my_backpack["软妹币"] += money
-                target_backpack["软妹币"] -= money
-            else:
-                msg = f"打劫失败,反被对方抢走了 软妹币 * {-money}"
-                my_backpack["软妹币"] += money
-                target_backpack["软妹币"] -= money
+            status, response = await http_client.post(
+                f"/users/{event.user_id}/robbery", json={"target_id": args_list[0]}
+            )
             await robbery.finish(
-                MessageSegment.reply(event.message_id) + msg)
+                MessageSegment.reply(event.message_id) + response["msg"]
+            )
         else:
             await robbery.finish(
-                MessageSegment.reply(event.message_id) + f"抢劫冷却中,{cd.left_time(event.user_id):.0f}秒")
+                MessageSegment.reply(event.message_id)
+                + f"抢劫冷却中,{cd.left_time(event.user_id):.0f}秒"
+            )
