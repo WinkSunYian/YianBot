c0927a22936632969afaa22811cebb7c1dfd09a8
SunYian <winksunyian@gmail.com>
2024-12-22 16:21:13 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   plugins/Help/__init__.py

--- plugins/Help/__init__.py
+++ plugins/Help/__init__.py
@@ -9,12 +9,12 @@ help = on_fullmatch("菜单", priority=5, block=True)
 
 @help.handle()
 async def _(event: MessageEvent):
-    msg_list = ["今日运势", "#签到", "摸", "#查Q绑", "#短信测压", "#电话测压"]
-    # for plugin in os.listdir("plugins/"):
-    #     if "_" in plugin:
-    #         continue
-    #     config = ConfigReader(plugin)
-    #     if "commands" in config:
-    #         for command in config["commands"]:
-    #             msg_list.append(command)
+    msg_list = [
+        "今日运势",
+        "签到",
+        "#背包",
+        "打劫",
+        "摸",
+    ]
+
     await help.finish(MessageSegment.reply(event.message_id) + "\n".join(msg_list))
