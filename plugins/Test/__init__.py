0165e5cedc0ef1a01dd416b87fe17de01f279009
SunYian <winksunyian@gmail.com>
2024-12-29 13:38:01 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   plugins/Test/__init__.py

--- plugins/Test/__init__.py
+++ plugins/Test/__init__.py
@@ -7,7 +7,7 @@ from nonebot.adapters.onebot.v11 import (
 )
 
 from utils.utils import DailyCountLimiter, BackpackControl
-
+from pathlib import Path
 from random import choice
 
 signin = on_fullmatch("#test", priority=6, block=True)
@@ -15,10 +15,5 @@ signin = on_fullmatch("#test", priority=6, block=True)
 
 @signin.handle()
 async def _(event: GroupMessageEvent):
-    path = "/data/YianBot/data/oxygen_card/fieldset_screenshot.png"
-    import os
-    if not os.path.exists(path):
-        print("文件不存在:", path)
-    else:
-        print("文件存在:", path)
+    path = Path("/data/YianBot/data/oxygen_card/fieldset_screenshot.png")
     await signin.finish(MessageSegment.image(path))
