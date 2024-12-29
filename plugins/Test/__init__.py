aed4ef56a86358614b32600cbb8f48c2f7e0d157
SunYian <winksunyian@gmail.com>
2024-12-29 13:32:16 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   plugins/Test/__init__.py

--- plugins/Test/__init__.py
+++ plugins/Test/__init__.py
@@ -15,5 +15,10 @@ signin = on_fullmatch("#test", priority=6, block=True)
 
 @signin.handle()
 async def _(event: GroupMessageEvent):
+    import os
+    if not os.path.exists(path):
+        print("文件不存在:", path)
+    else:
+        print("文件存在:", path)
     path = "/data/YianBot/data/oxygen_card/fieldset_screenshot.png"
     await signin.finish(MessageSegment.image(path))
