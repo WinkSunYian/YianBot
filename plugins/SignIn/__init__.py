3a62d9ed0685f4c440933981afdaf13d1c6a4ac8
SunYian <winksunyian@gmail.com>
2025-01-10 15:16:04 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .gitignore
    	modified:   plugins/SignIn/__init__.py

--- plugins/SignIn/__init__.py
+++ plugins/SignIn/__init__.py
@@ -13,4 +13,4 @@ signin = on_command("#签到", aliases={"签到"}, priority=6, block=True)
 @signin.handle()
 async def _(event: GroupMessageEvent, tags: list = Depends(get_user_tags)):
     status, response = await http_client.put(f"/users/{event.user_id}/sign-in")
-    await signin.finish(MessageSegment.reply(event.message_id) + response["msg"])
+    await signin.finish(MessageSegment.reply(event.message_id) + response["message"])
